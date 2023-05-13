be580852d3d12a42c5bbf974a44c21937f09dfe0
mohilcode <mohil.garg13@gmail.com>
2023-05-13 14:43:46 +0900

    Initial commit

---
+++ server.js
@@ -0,0 +1,39 @@
+const puppeteer = require('puppeteer');
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/scrape', async (req, res) => {
+  // Use the new headless mode
+  const browser = await puppeteer.launch({ headless: 'new' });
+  const page = await browser.newPage();
+
+  // Use the barcode from the query parameter
+  const barcode = req.query.barcode;
+  if (!barcode) {
+    res.status(400).send("Please provide a barcode.");
+    return;
+  }
+
+  await page.goto(`https://www.tajimaya-cc.net/?s=${barcode}&search-type=products&submit=`);
+
+  // Extract the product URL from the search result
+  const productURL = await page.$eval('ul.prod_list li a', el => el.href);
+
+  // Navigate to the product page
+  await page.goto(productURL);
+
+  // Extract the product name and description
+  const [productName, productDescription] = await Promise.all([
+    page.$eval('.tit_prod .tit_txt', el => el.textContent),
+    page.$eval('#overview', el => el.textContent.trim())
+  ]);
+
+  await browser.close();
+
+  res.json({ productName, productDescription });
+});
+
+app.listen(port, () => {
+  console.log(`Server is running at http://localhost:${port}`);
+});
