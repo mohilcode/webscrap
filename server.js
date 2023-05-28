cea226e81ca75a766b7020cede75b03762c76dce
mohilcode <112541468+mohilcode@users.noreply.github.com>
2023-05-28 16:08:06 +0900

    Update server.js

--- server.js
+++ server.js
@@ -1,14 +1,12 @@
 const puppeteer = require('puppeteer');
 const express = require('express');
 const app = express();
-const port = process.env.PORT || 3000; // Use environment variable for port if available
+const port = process.env.PORT || 3000;
 
 app.get('/scrape', async (req, res) => {
-  // Use the new headless mode
   const browser = await puppeteer.launch({ headless: 'new' });
   const page = await browser.newPage();
 
-  // Use the barcode from the query parameter
   const barcode = req.query.barcode;
   if (!barcode) {
     res.status(400).send("Please provide a barcode.");
@@ -17,16 +15,12 @@ app.get('/scrape', async (req, res) => {
 
   await page.goto(`https://www.tajimaya-cc.net/?s=${barcode}&search-type=products&submit=`);
 
-  // Extract the product URL from the search result
   const productURL = await page.$eval('ul.prod_list li a', el => el.href);
 
-  // Navigate to the product page
   await page.goto(productURL);
 
-  // Extract the product name
   const productName = await page.$eval('.tit_prod .tit_txt', el => el.textContent);
 
-  // Check if the #overview element exists and extract its content, default to "Description not available"
   let productDescription = 'Description not available';
   if (await page.$('#overview') !== null) {
     productDescription = await page.$eval('#overview', el => el.textContent.trim());
