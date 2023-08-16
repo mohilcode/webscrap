d68eac33e24715f624e8e4c23b79fcadb98e9d0c
Mohil Garg <112541468+mohilcode@users.noreply.github.com>
2023-08-17 02:53:49 +0900

    Update server.js

--- server.js
+++ server.js
@@ -13,37 +13,40 @@ const navigateWithTimeout = async (page, url, timeout) => {
 };
 
 app.get('/scrape', async (req, res) => {
-  const browser = await puppeteer.launch({ args: ['--no-sandbox', '--disable-setuid-sandbox'], headless: "new" });
+  const browser = await puppeteer.launch({ args: ['--no-sandbox', '--disable-setuid-sandbox'], headless: true });
   const page = await browser.newPage();
 
   const barcode = req.query.barcode;
   if (!barcode) {
+    await browser.close();
     res.status(400).send("Please provide a barcode.");
     return;
   }
 
   try {
     await navigateWithTimeout(page, `https://www.tajimaya-cc.net/?s=${barcode}&search-type=products&submit=`, 15000);
+    
+    const productURL = await page.$eval('ul.prod_list li a', el => el.href);
+    await page.goto(productURL);
+    const productName = await page.$eval('.tit_prod .tit_txt', el => el.textContent);
+    let productDescription = 'Description not available';
+    
+    if (await page.$('#overview') !== null) {
+      productDescription = await page.$eval('#overview', el => el.textContent.trim());
+    }
+
+    await browser.close();
+    res.json({ productName, productDescription });
+
   } catch (error) {
     await browser.close();
-    res.status(500).send("Navigation timeout or an error occurred.");
+    if (error.message === 'Navigation timeout') {
+      res.status(408).send(null); // Return null for timeout as you mentioned.
+    } else {
+      res.status(500).send("An error occurred while fetching product info.");
+    }
     return;
   }
-
-  const productURL = await page.$eval('ul.prod_list li a', el => el.href);
-
-  await page.goto(productURL);
-
-  const productName = await page.$eval('.tit_prod .tit_txt', el => el.textContent);
-
-  let productDescription = 'Description not available';
-  if (await page.$('#overview') !== null) {
-    productDescription = await page.$eval('#overview', el => el.textContent.trim());
-  }
-
-  await browser.close();
-
-  res.json({ productName, productDescription });
 });
 
 app.listen(port, () => {
